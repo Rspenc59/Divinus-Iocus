bcf19bd60aea3860ae19d0bf7064138061d6c7b0
hcrossl2 <hcrossl2@bruinmail.slcc.edu>
2016-04-27 09:50:34 -0600

    Added files via upload

---
+++ savehud/src/save/Data.java
@@ -0,0 +1,10 @@
+package save;
+
+public class Data {
+	public static int hp = 5;
+	public static int xmap = 0; 
+	public static int ymap = 0;
+	public static boolean level1 = false;
+	public static boolean level2 = false;
+	public static boolean level3 = false;
+}
